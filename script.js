bc9c5f493a90fa4b46b878d933866f55852de428
Raissa Aciole <159713895+raissaaciolee@users.noreply.github.com>
2024-09-17 19:05:23 -0300

    final

---
+++ script.js
@@ -0,0 +1,61 @@
+const buttonAddTask = document.querySelector("#buttonAddTask")
+const inputTask = document.querySelector("#inputTask")
+const containerTasks = document.querySelector(".tasks-container")
+const modal = document.querySelector(".modal-delete")
+
+let containerList = []
+
+const addTaskInList = (event) => {
+    event.preventDefault()
+
+    if(inputTask.value == "") {
+        alert("A lista não pode estar vazia")
+        return
+    }
+
+    containerList.push(inputTask.value)
+
+    inputTask.value = ""
+    inputTask.focus()
+    showTask()
+}
+
+const showTask = () => {
+    let newTask = ""
+
+    if(containerList.length != 0) {
+        containerList.forEach((task, i) => {
+            newTask = newTask + `
+                    <li class="task flex items-center gap-12">
+                        <div class="checkbox-wrapper">
+                            <div class="icon-checkbox"></div>
+                            <input type="checkbox" name="" id="">
+                        </div>
+                        <span>${task}</span>
+                        <button type="button" onclick='deleteTask(${i})'>
+                            <img src="./assets/trash.svg" alt="">
+                        </button>
+                    </li>
+            `
+        })
+    }
+
+    containerTasks.innerHTML = newTask
+}
+
+const deleteTask = (i) => {
+    containerList.splice(i, 1)
+    showTask()
+
+    modal.classList.remove("hide-modal")
+    modal.querySelector("button").onclick = (event) => {
+        event.preventDefault()
+        modal.classList.add("hide-modal")
+    }
+
+    setTimeout(function() {
+        modal.classList.add("hide-modal")
+    }, 3000)
+}
+
+buttonAddTask.addEventListener("click", addTaskInList)
